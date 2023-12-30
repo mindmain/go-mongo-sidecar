f229bc7ff1fecc4d30773a113b7ec5cf0e44c438
Nico <spinanico93@gmail.com>
2023-12-30 15:58:18 +0100

    fix | order id of members replica

--- db/utils.go
+++ db/utils.go
@@ -1,14 +1,33 @@
 package db
 
+import (
+	"strconv"
+	"strings"
+)
+
 func hostsToMembers(hosts []string) []*replicaSetMember {
 	var members []*replicaSetMember
 
-	for i, host := range hosts {
+	for _, host := range hosts {
 		members = append(members, &replicaSetMember{
-			ID:   i,
+			ID:   getNumberStatefulSet(host),
 			Host: host,
 		})
 	}
 
 	return members
+}
+
+func getNumberStatefulSet(host string) int {
+
+	hostname := strings.Split(host, ".")[0]
+
+	number, err := strconv.Atoi(strings.Split(hostname, "-")[len(strings.Split(hostname, "-"))-1])
+
+	if err != nil {
+		return 0
+	}
+
+	return number
+
 }
