cfa55867093ab23e28ccd9212c5fd586fe1b4696
Nico <spinanico93@gmail.com>
2023-12-30 15:46:41 +0100

    add | more better logs

--- service/types.go
+++ service/types.go
@@ -126,11 +126,11 @@ func (s *sidecarService) Run(ctx context.Context) error {
 						if morePodsOfMembers || lessPodsOfMembers {
 
 							if morePodsOfMembers {
-								log.Println("[INFO] more pods of members")
+								log.Printf("[INFO] more pods of members, pods: %d members: %d ", len(hosts), mongoMembersLive)
 							}
 
 							if lessPodsOfMembers {
-								log.Println("[INFO] less pods of members")
+								log.Printf("[INFO] less pods of members, pods: %d members: %d ", len(hosts), mongoMembersLive)
 							}
 
 							if err := s.mongoHandler.ReplicaSetReconfig(ctx, hosts); err != nil {
